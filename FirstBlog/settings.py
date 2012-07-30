c6d9789814869fa4c8be2a7b1bb13496fae22ce9
Rick <leerick@gmail.com>
2012-07-30 16:46:29 +1000

    fixed syntax error in setting.py

--- FirstBlog/settings.py
+++ FirstBlog/settings.py
@@ -13,8 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
 		dj_database_url.config(default='postgres://localhost')
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 
-															# 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', mysql, etc.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
