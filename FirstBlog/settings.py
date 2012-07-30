1c9c3aee391d8dae53fe312e6313a1671241350c
Rick <leerick@gmail.com>
2012-07-30 13:41:29 +1000

    fixed settings.py file typo

--- FirstBlog/settings.py
+++ FirstBlog/settings.py
@@ -13,7 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
 		dj_database_url.config(default='postgres://localhost')
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 
+															# 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
